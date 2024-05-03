e8f96607133aa544da8912c61573ab0f74704eb4
Apu Chandra Das <apuchandradas5568@gmail.com>
2024-05-03 13:29:04 +0530

    first commit

---
+++ src/model/User.ts
@@ -0,0 +1,50 @@
+import mongoose, {Schema, Document} from "mongoose";
+
+
+export interface Message extends Document{
+    content: string;
+    createdAt: Date
+}
+
+const MessageSchema: Schema<Message> = new Schema  ({
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        required: true
+    }
+})
+
+
+
+export interface User extends Document{
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    isAcceptingMessage: boolean;
+    message: Message[]
+}
+
+const UserSchema: Schema<User> = new Schema  ({
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        match: [
+            /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+            "Please use a valid email address"
+        ],
+
+    }
+})
